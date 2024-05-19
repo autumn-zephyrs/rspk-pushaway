830a31b745a9e37001c996c3e97c2499409d5c8b
AutumnZephyrs <autumn.zephyrs@gmail.com>
2024-05-19 03:30:40 +0100

    removed login routes

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -50,17 +50,7 @@
 
                         </a>
                     </div>
-                    <div class="">
-                        @auth
-                            <a href="#" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Log out</a>
-                        @else
-                            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Log in</a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Register</a>
-                            @endif
-                        @endauth
-                    </div>
+            
                 </div>
             @endif
         </nav>
