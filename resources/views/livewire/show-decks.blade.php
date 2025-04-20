54859d05b4e8cbf958916c7d85c00738a3e350e2
Autumn Lamb <autumn.zephyrs@gmail.com>
2025-04-20 11:26:13 +0100

    "other" == no rights

--- resources/views/livewire/show-decks.blade.php
+++ resources/views/livewire/show-decks.blade.php
@@ -82,7 +82,7 @@
                             <div class="col-span-5 flex items-center gap-2 border-r border-holon-400 lg:py-1 py-2 justify-between">
                                 <div class="text-sm lg:text-base">{{isset($deck->deckType) ? $deck->deckType->name : 'notfound'}}</div>
                                 <div class="flex gap-2 pr-4">
-                                    @if($deck->identifier !== 'substitute')
+                                    @if($deck->identifier !== 'other')
                                         <img class="max-h-4 lg:max-h-6" src="https://limitlesstcg.s3.us-east-2.amazonaws.com/pokemon/gen9/{{$deck->deckType->icon_primary}}.png">
                                         @if($deck->deckType->icon_secondary)
                                             <img class="max-h-4 lg:max-h-6" src="https://limitlesstcg.s3.us-east-2.amazonaws.com/pokemon/gen9/{{$deck->deckType->icon_secondary}}.png">
