f0af1cfe59d3e996a4d220880fd2b779455f8b47
autumn-zephyrs <autumn.zephyrs@gmail.com>
2026-02-23 22:47:00 +0000

    made getTournaments a daily command

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+Schedule::command('get:tournaments')->daily();
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
